0c74ebf7ed8842e65853fbb72b052eb608ed8b37
saraazizi7495 <sazizi@uva.nl>
2024-08-18 14:31:54 +0200

    added new line

--- c++.cpp
+++ c++.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(){
     string name = "Sara Azizi";
     string name2 = "Azizi Sara";
+    string age = "28";
     cout << name << endl;
     cout << name2 << endl;
 return 0;    
